9f13d4a704f13f2b6982043b548c5f475e4073a5
vladimir <vladimirsostaric3390@gmail.com>
2018-03-14 10:17:41 +0100

    Create entity

--- src/main/java/app/model/Person.java
+++ src/main/java/app/model/Person.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -12,13 +16,28 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Person {
 
+    @Id
+    private int id;
+
+    @Column(name = "full_name")
     private String fullName;
+
+    @Column
     private String address;
+
+    @Column(name = "date_of_birth")
     private LocalDate dateOfBirth;
+
+    @Column(name = "number_of_visits")
     private int numberOfVisits;
+
+    @Column(name = "money_on_his_mind")
     private BigDecimal moneyOnHisMind;
+
+    @Transient
     private Car car;
 
 }
