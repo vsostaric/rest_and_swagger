fea110501073c3d63a64086dbeed78f25f63ed7c
vladimir <vladimirsostaric3390@gmail.com>
2018-03-15 16:27:55 +0100

    Optimize imports

--- src/main/java/app/controller/PersonController.java
+++ src/main/java/app/controller/PersonController.java
@@ -5,7 +5,10 @@ import app.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
--- src/main/java/app/service/impl/PersonServiceImpl.java
+++ src/main/java/app/service/impl/PersonServiceImpl.java
@@ -5,7 +5,6 @@ import app.repository.PersonRepository;
 import app.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
